1f3d33474d7381d7e29396b5f2a25c20862fc4d1
Poli <57406032+ovopoli@users.noreply.github.com>
2020-01-30 10:18:40 -0600

    binary search TP attempt

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -12,4 +12,24 @@
 
 var binarySearch = function(array, target) {
 
-};
+    for(let i = 0; i < array.length; i++) {
+        if(target === undefined || target > array[i]) {
+            return null;
+        }
+        if(target === array[i]) {
+            return array[i];
+        }
+    }
+}
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 10));
+
+//     if(target > array.length || target === undefined) {
+//         return null;
+//     }
+//     for(let i = 0; i < array.length; i++) {
+//         if(array[i] === target) {
+//             return array[i];
+//         }
+//     }
+// };
+// console.log(binarySearch([1,2,3,4,5,6,7,8,9,10], 1));
