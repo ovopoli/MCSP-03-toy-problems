8d114705ddea373e0056a2760df1e9cfb016d693
Poli <57406032+ovopoli@users.noreply.github.com>
2020-03-17 10:27:30 -0500

    attempt

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -6,4 +6,27 @@
  * whitespace on each side of dad).
  */
 
-var longestPalindrome = function(string) {};
+var longestPalindrome = function(string) {
+    //will store the longest palindrome
+    let currPalindrome = '';
+    //keeps the strings last index element
+    let i = string.length;
+    // used to identify the strings first element
+    let y = 0;
+
+    //helper funtion to recurse that will check the first element to every element starting from the back
+    letterChecker = function (string) {
+        if (string[y] !== string[i]) {
+            i--;
+        }
+    }
+    // if the elements match, move in and check the next one
+    while (string[y] === string[i]) {
+        //make the currPalindrome string have the elements that are matching
+        currPalindrome[y] = string[y];
+        currPalindrome[currPalindrome.length + 1] = string[i];
+        y++;
+        i--;
+    } 
+};
+
