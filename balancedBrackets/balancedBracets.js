85049b19c467ee24a95de371a955b41edf598a9e
Poli <57406032+ovopoli@users.noreply.github.com>
2020-03-19 09:54:59 -0500

    attempt

--- balancedBrackets/balancedBracets.js
+++ balancedBrackets/balancedBracets.js
@@ -9,4 +9,20 @@
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+const isBalanced = str => {
+    if (str.includes('(') & str.includes(')')) {
+        return true;
+    }   else if 
+            (str.includes('[') & str.includes(']')) {
+                return true;
+            }
+        else if 
+            (str.includes('{') & str.includes('}')) {
+                return true;
+            }
+     else {
+        return false;
+    }
+};
+
+console.log(isBalanced('({ })'))
